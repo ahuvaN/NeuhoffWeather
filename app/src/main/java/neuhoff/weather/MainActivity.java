04d0c26571e30812488a2e021b53ef467d0ca465
Ahuva <ahuvanewoff112@gmail.com>
2016-01-15 00:19:20 -0500

    working android app

--- app/src/main/java/neuhoff/weather/MainActivity.java
+++ app/src/main/java/neuhoff/weather/MainActivity.java
@@ -1,43 +1,52 @@
 package neuhoff.weather;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText zip;
-    private Button button;
+    private ViewPager viewPager;
+    private SharedPreferences preferences;
+    private ArrayList<String> locations;
+    WeatherLocationsPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.weather_activity);
+        preferences = this.getSharedPreferences("default", MODE_PRIVATE);
 
-        zip = (EditText) findViewById(R.id.inputZip);
-        button = (Button) findViewById(R.id.cont);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startIntent();
-            }
-        });
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        locations = new ArrayList<String>();
+        pagerAdapter = new WeatherLocationsPagerAdapter(viewPager, locations, this);
+        viewPager.setAdapter(pagerAdapter);
     }
 
-    private void startIntent() {
-        Intent intent = new Intent(this, WeatherActivity.class);
-        int zip = Integer.parseInt(this.zip.getText().toString());
-        intent.putExtra("Zip", zip);
-        startActivity(intent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < locations.size(); i++){
+            sb.append(locations.get(i) + ";");
+        }
+        editor.putString("ZIPS", sb.toString().trim());
+        editor.apply();
     }
 
+    @Override
+     public void onResume() {
+        super.onResume();
+        String temp1 = preferences.getString("ZIPS", "11210");
+        String[] temp2 = temp1.split(";");
+        for (String s : temp2) {
+            locations.add(s);
+            pagerAdapter.notifyDataSetChanged();
+        }
+    }
 
 }
